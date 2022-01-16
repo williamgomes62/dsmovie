cd016bf621b9155d802de509e9c09693f021dbed
William Gomes <williamlimagomes62@gmail.com>
2022-01-16 15:34:31 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,16 @@
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import './styles.css';
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+      console.log(response.data);
+    })
+
   return (
     <>
       <Pagination />
